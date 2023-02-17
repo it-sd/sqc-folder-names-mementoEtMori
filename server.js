e519ffddd7e473016d32a8ed0116b02b50c9a9ad
Braden Walbeck <bwalbeck@student.cvtc.edu>
2023-02-16 20:17:35 -0600

    Works only with linear folder names

--- server.js
+++ server.js
@@ -1,3 +1,31 @@
 const newFolderName = function (folders) {
 
-}
+  
+  if (folders.length === 0) {
+    return "New Folder"
+  } else if (folders[0] === "New Folder" && folders.length === 1) {
+    return "New Folder (2)"
+  } else {
+  
+      for (let i = folders.length; i > 0; i--) {
+        let n = folders.length;
+        if (folders[i] === "New Folder (" + (n) + ")") {
+          return "New Folder (" + (n+1) + ")"
+        } else {
+          n++
+        }
+      
+      }
+      
+    }
+  
+  }
+
+
+console.log(newFolderName([]));
+console.log(newFolderName(["New Folder"]));
+console.log(newFolderName(["New Folder", "New Folder (2)"]));
+console.log(newFolderName(["New Folder", "New Folder (2)", "New Folder (3)"]));
+
+
+module.exports = { newFolderName }
